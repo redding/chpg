bb77bccbf8b7e36f9f5ca86bb3b4930c5c046863
Kelly Redding <kelly@kellyredding.com>
2020-03-03 08:43:25 -0600

    add initial API and implementation
    This project is based heavily off of the work we've done with
    [Rb](https://github.com/redding/rb).
    
    This adds the main CLI and sub-command implementations and the
    scripts to release, install, and uninstall the application.
    
    The API is very simiilar to Rb except it works off of the installed
    Postgres.app versions found in
    `/Applications/Postgres.app/Contents/Versions`. All this does is
    modify PATH to include the specified version's bin path. It has
    features similar to Rb including: status output, listing available
    versions, and `.chpg-version` file support. It also supports auto
    updating versions on changing directories so you can use a
    `.chpg-version` file to lock in the Postgres version for individual
    projects.
    
    I found myself wanting this as I want to lock in certain projects
    to a specific version of Postgres and control when/if I upgrade.
    However, I don't control the Postgres version requirements in other
    projects and need to be able to use a different version on those
    projects.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+CHPG_HOME_DIR="$HOME/.chpg"
+CHPG_RELEASE="0.0.1.beta1"
+
+# make sure the bin path is in place
+
+      [ -n "$PREFIX" ] || PREFIX="/usr/local"
+      BIN_PATH="$PREFIX/bin"
+      mkdir -p "$BIN_PATH"
+
+# download the release tag and link to the bin path
+
+      mkdir -p "$CHPG_HOME_DIR"
+      pushd "$CHPG_HOME_DIR" > /dev/null &&
+        rm -rf "chpg-$CHPG_RELEASE"
+        curl -L "https://github.com/redding/chpg/tarball/$CHPG_RELEASE" | tar xzf - '*/libexec/*'
+        mv *-chpg-* "chpg-$CHPG_RELEASE"
+        ln -sf "chpg-$CHPG_RELEASE/libexec"
+      popd > /dev/null
+
+# install in the bin path
+
+      ln -sf "$CHPG_HOME_DIR/libexec/chpg" "$BIN_PATH"
+
+# done!
+
+      echo "Installed at ${BIN_PATH}/chpg"
